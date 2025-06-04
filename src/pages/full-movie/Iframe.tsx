3a34d2e0c9071e9bc1a3d1893125f99268f5a8e4
Nejc-GM <nejc.gjura-meke@inova.si>
2025-06-04 16:00:25 +0200

    Merged iframe into main

--- src/pages/full-movie/Iframe.tsx
+++ src/pages/full-movie/Iframe.tsx
@@ -3,20 +3,12 @@ import { useSearchParams } from "react-router-dom";
 import { fetchSpecific, FetchSpecificResponse } from "../../functions/Fetching.js";
 import BackButton from "../../components/BackButton";
 import ShareButton from "../../components/ShareButton.js";
-<<<<<<< HEAD
-=======
-import ProviderComponent from "./components/ProviderComponent.js";
->>>>>>> 1
 
 const Iframe = () => {
   const [info, setInfo] = useState<FetchSpecificResponse>();
   const [searchParams] = useSearchParams();
   const movieId = searchParams.get("id");
   const type = searchParams.get("type");
-<<<<<<< HEAD
-=======
-  const [currentProviderUrl, setCurrentProviderUrl] = useState('');
->>>>>>> 1
 
   useEffect(() => {
     const loadInfo = async () => {
@@ -37,31 +29,14 @@ const Iframe = () => {
         </div>
         <ShareButton />
       </div>
-<<<<<<< HEAD
       <div className=" w-full aspect-[16/9]">
         <iframe
           className="w-full h-full"
           src={`https://vidsrc.pm/embed/${type}/${movieId}`}
-=======
-      <div className=" w-full aspect-[16/9] max-w-[1200px] mx-auto relative">
-        <iframe
-          className="w-full h-full"
-          src={currentProviderUrl}
->>>>>>> 1
           referrerPolicy="origin"
           frameBorder="0"
           allowFullScreen
         ></iframe>
-<<<<<<< HEAD
-=======
-        <ProviderComponent
-        newProvider={setCurrentProviderUrl}
-        title="Choose a Provider"
-        movieId={movieId!}
-        type={type!}
-        className="mt-[8px]"
-      />
->>>>>>> 1
       </div>
     </>
   );
