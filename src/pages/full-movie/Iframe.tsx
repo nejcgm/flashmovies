269fc60112cde54161127f69b3e6ad17c46a9c25
Nejc-GM <nejc.gjura-meke@inova.si>
2025-06-05 09:15:04 +0200

    adBlock download button

--- src/pages/full-movie/Iframe.tsx
+++ src/pages/full-movie/Iframe.tsx
@@ -1,16 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
-import { fetchSpecific, FetchSpecificResponse } from "../../functions/Fetching.js";
+import {
+  fetchSpecific,
+  FetchSpecificResponse,
+} from "../../functions/Fetching.js";
 import BackButton from "../../components/BackButton";
 import ShareButton from "../../components/ShareButton.js";
+import ProviderButton from "./components/ProviderButton.js";
 import ProviderComponent from "./components/ProviderComponent.js";
 
 const Iframe = () => {
   const [info, setInfo] = useState<FetchSpecificResponse>();
   const [searchParams] = useSearchParams();
-  const [currentProviderUrl, setCurrentProviderUrl] = useState('');
+  const [currentProviderUrl, setCurrentProviderUrl] = useState("");
   const movieId = searchParams.get("id");
   const type = searchParams.get("type");
+  const addBlockUrl = "https://chrome.google.com/webstore/detail/adblock-plus-free-ad-bloc/cfhdojbkjhnklbpkdaibdccddilifddb";
 
   useEffect(() => {
     const loadInfo = async () => {
@@ -24,12 +29,24 @@ const Iframe = () => {
 
   return (
     <>
-      <div className="flex gap-4 items-center  mb-[24px]">
-        <BackButton />
-        <div className="font-roboto text-[48px] text-white">
-          {info?.title || info?.name}
+      <div className="flex font-roboto  text-white justify-between">
+        <div className="flex gap-4 items-center  mb-[24px]">
+          <BackButton />
+          <div className="text-[48px]">
+            {info?.title || info?.name}
+          </div>
+          <ShareButton />
         </div>
-        <ShareButton />
+        <div className="flex flex-col gap-1 items-center">
+          <div className="text-[22px] font-bold">Recommended Adblock</div>
+          <a
+            href={addBlockUrl}
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <ProviderButton provider="Download" className={"bg-gray-500"} />
+          </a>
+        </div>
       </div>
       <div className=" w-full aspect-[16/9] max-w-[1200px] mx-auto relative">
         <iframe
@@ -40,12 +57,12 @@ const Iframe = () => {
           allowFullScreen
         ></iframe>
         <ProviderComponent
-        newProvider={setCurrentProviderUrl}
-        title="Choose a Provider"
-        movieId={movieId!}
-        type={type!}
-        className="mt-[8px]"
-      />
+          newProvider={setCurrentProviderUrl}
+          title="Choose a Provider"
+          movieId={movieId!}
+          type={type!}
+          className="mt-[8px]"
+        />
       </div>
     </>
   );
--- src/pages/info-Page-Components/MediaComponent.tsx
+++ src/pages/info-Page-Components/MediaComponent.tsx
@@ -60,7 +60,7 @@ const HeaderInfo: React.FC<HeaderInfoProps> = ({
                 <path d="M12 24C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12zm0-1c6.075 0 11-4.925 11-11S18.075 1 12 1 1 5.925 1 12s4.925 11 11 11z"></path>
               </svg>
             
-            <div className="text-[16px] sm:text-[20px]">play {type == "movie" ? "movie" : "series"}</div>
+            <div className="text-[16px] sm:text-[20px]">Watch {type == "movie" ? "movie" : "series"}</div>
             </div>
           </div>
         </button>
