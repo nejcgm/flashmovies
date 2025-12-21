8a8e09f83c97d3e25fa0831302fcb65b61e6fe22
Nejc-GM <nejc.gjura-meke@inova.si>
2025-12-21 11:49:34 +0100

    add some xmas spirit :)

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,10 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css'; 
 import App from './App';
 import AdsterraRedirect from './components/AdsterraRedirect';
-
 import { getAdsterraConfig } from './config/adsterraConfig';
 import { AdTrackerProvider } from './context/AdTrackerContext';
 import { HelmetProvider } from 'react-helmet-async';
+import { Snowfall } from 'react-snowfall';
 //import PopupScriptManager from './components/PopupScriptManager';
 
 function Root() {
@@ -16,6 +16,13 @@ function Root() {
   return (
     <HelmetProvider>
     <AdTrackerProvider>
+      <Snowfall
+      snowflakeCount={110}      
+      speed={[0.9, 1.7]}        
+      radius={[1, 2]}       
+      opacity={[0.15, 0.3]}     
+      wind={[-0.2, 0.6]}        
+    />
       <AdsterraRedirect enabled={adsterraConfig.enabled} />
       <App />
       {/* <PopupScriptManager /> */}
